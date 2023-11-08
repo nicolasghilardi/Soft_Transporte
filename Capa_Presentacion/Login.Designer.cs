953ac34e04e3a5ca4dc72a9857d76cd0a008ca0b
Nicolas Ghilardi <nghilardi.93@gmail.com>
2023-11-08 15:12:29 -0300

    Desarrollo del Login

--- Capa_Presentacion/Login.Designer.cs
+++ Capa_Presentacion/Login.Designer.cs
@@ -28,18 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnIniciar = new Button();
+            checkBox1 = new CheckBox();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = Properties.Resources.Fondo_Login;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(944, 476);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(348, 192);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 27);
+            label1.TabIndex = 1;
+            label1.Text = "Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Century Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(348, 269);
+            label2.Name = "label2";
+            label2.Size = new Size(148, 27);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(348, 222);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(286, 32);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(348, 299);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(286, 32);
+            textBox2.TabIndex = 4;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 192, 0);
+            btnIniciar.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 192, 0);
+            btnIniciar.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            btnIniciar.Location = new Point(537, 337);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(97, 34);
+            btnIniciar.TabIndex = 5;
+            btnIniciar.Text = "Iniciar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.ForeColor = Color.White;
+            checkBox1.Location = new Point(348, 337);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(160, 24);
+            checkBox1.TabIndex = 6;
+            checkBox1.Text = "Mostrar Contraseña";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Font = new Font("Century Gothic", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSalir.Location = new Point(12, 430);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(97, 34);
+            btnSalir.TabIndex = 7;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(944, 476);
+            Controls.Add(btnSalir);
+            Controls.Add(checkBox1);
+            Controls.Add(btnIniciar);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
+            Load += Login_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnIniciar;
+        private CheckBox checkBox1;
+        private Button btnSalir;
     }
 }
--- Capa_Presentacion/Login.cs
+++ Capa_Presentacion/Login.cs
@@ -6,5 +6,25 @@ namespace Capa_Presentacion
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.label1.Parent = this.pictureBox1;
+            this.label1.BackColor = Color.Transparent;
+            this.label2.Parent = this.pictureBox1;
+            this.label2.BackColor = Color.Transparent;
+            this.checkBox1.Parent = this.pictureBox1;
+            this.checkBox1.BackColor = Color.Transparent;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
